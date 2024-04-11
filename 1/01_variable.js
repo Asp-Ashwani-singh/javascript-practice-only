4f13893e602b93771f6d38888b4deba48a1bb749
Ashwani singh <rrvapmr@gmail.com>
2024-04-12 00:24:15 +0530

    we have tested on data type

---
+++ 1/01_variable.js
@@ -0,0 +1,14 @@
+const accountId=45445;
+let accountEmail="rrvapmr@gmail.com";
+var accountPassword="password";
+accountCity="New Delhi";
+let accountState;
+
+//accountId=454545545; //not allow
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+/*
+we can't use var 
+because of issue of bloack and functional scope
+
+*/
---
+++ 1/02_datatypes.js
@@ -0,0 +1,10 @@
+
+//number 
+//bigint
+//string
+//boolean
+//null =>standalone value
+//undefined=>
+//symbol =>unique ness
+
+console.table([typeof(null),typeof(undefined),typeof(NaN)] )
